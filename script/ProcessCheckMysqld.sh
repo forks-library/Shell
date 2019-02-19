b0d8bfacfa29c6bf9ffd5556e6a200c9c241d7ca
PersiLiao <xiangchu.liao@gmail.com>
2019-02-19 22:01:06 +0800

    fixed process check.sh dir
    fixed process check.sh dir

--- script/ProcessCheckMysqld.sh
+++ script/ProcessCheckMysqld.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-source /home/persi/crontab/shell/ProcessCheck.sh
+source ./ProcessCheck.sh
 # 10秒钟检测一次nginx
 ProcessCheck 'mysqld' 'systemctl start mysqld.service' 10;
--- script/ProcessCheckPhp.sh
+++ script/ProcessCheckPhp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-source /home/persi/crontab/shell/ProcessCheck.sh
+source ./ProcessCheck.sh
 # 10秒钟检测一次nginx
 ProcessCheck 'php-fpm' 'systemctl start php-fpm.service' 10;
